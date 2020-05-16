915248f6d81cf3bcc7949048eabad27b17298510
flfelipelopes <fl.felipelopes288@gmail.com>
2020-05-16 09:26:35 -0300

    Exercício de Arrays, Dictionaries, Console, Alerts

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
-var nome = "Felipe Lopes";
-var ano = "2020";
-alert("Bem-vindo a página de " + nome + ".\n" + 
-"Estamos no ano de " + ano);
+//var nome = "Felipe Lopes";
+//var ano = "2020";
+//alert("Bem-vindo a página de " + nome + ".\n" + 
+//"Estamos no ano de " + ano);
+
+//var frutas = [{nome:"maça", cor:"vermelho"},
+//{nome: "abacaxi", cor: "amarelo"}] // Array de dicionários
+//console.log(frutas); // log da array de dicionário
+//alert(frutas[0].cor); // exibe alert da var cor do index 0 - maça
+
+//var frutas = {nome: "maça", cor:"vermelho"} // Dicionário
+//console.log(frutas);
+//alert("Meu coração é " + frutas.cor)
+
 
 //console.log(ano.replace("2020","2021")); // substitui a String no log
 
-var lista = ["maça", "pêra", "laranja"]
-lista.push("uva") // inclui
-console.log(lista[1]) // exibe parametro da variavél no console do navegador
-lista.push("abacaxi"); // inclui
-lista.pop("abacaxi"); // exclui
+//var lista = ["maça", "pêra", "laranja"]
+//lista.push("uva") // inclui
+//console.log(lista[1]) // exibe parametro da variavél no console do navegador
+//lista.push("abacaxi"); // inclui
+//lista.pop("abacaxi"); // exclui
 
+//console.log(lista.length); // tamanho da array
+//console.log(lista.reverse); // inverte ordem da array
+//console.log(lista.toString); // conver array em String
+
+//console.log(lista.join(" - ")) // adiciona o - entre a array e transforma em String
