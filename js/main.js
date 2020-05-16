6a116e3e63319d5d23b09bb310c6970513d3c30a
flfelipelopes <fl.felipelopes288@gmail.com>
2020-05-16 09:01:55 -0300

    Criado arquivo .js para integração ao index.html

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var nome = "Felipe Lopes";
+var ano = "2020";
+alert("Bem-vindo a página de " + nome + ".\n" + 
+"Estamos no ano de " + ano);
+
+//console.log(ano.replace("2020","2021")); // substitui a String no log
+
+var lista = ["maça", "pêra", "laranja"]
+lista.push("uva") // inclui
+console.log(lista[1]) // exibe parametro da variavél no console do navegador
+lista.push("abacaxi"); // inclui
+lista.pop("abacaxi"); // exclui
+
